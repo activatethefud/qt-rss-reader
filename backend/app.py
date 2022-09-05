c1c3f3a2fbfaffb65eb57438b836ea48fe0f8853
Nikola Sutic <nikolasutic@protonmail.com>
2022-09-06 01:36:34 +0200

    Added Feed delete. Fixed DB connect configx.

--- backend/app.py
+++ backend/app.py
@@ -6,13 +6,30 @@ app = Flask(__name__)
 @app.route('/new_feed', methods = ["GET"])
 def new_feed():
         new_feed_url = request.args["url"]
-        db.add_new_feed(new_feed_url)
 
-        return new_feed_url
+        try:
+                db.add_new_feed(new_feed_url)
+        except:
+                return "Db error", 500
+
+        return new_feed_url, 200
+
+@app.route('/delete_feed', methods = ["GET"])
+def delete_feed():
+        feed_url = requests.args["url"]
+
+        try:
+                db.delete_feed(feed_url)
+        except:
+                return "Db error", 500
+
+        return feed_url, 200
 
 @app.route("/")
 def hello_world():
         return 'Hello, world!'
 
-if __name__ == "__main__":
-        db.init()
+db.init()
+
+#if __name__ == "__main__":
+#        db.init()
--- backend/db.py
+++ backend/db.py
@@ -3,18 +3,27 @@ import sys
 from config import *
 
 conn = mariadb.connect(
-        user=DB_USER
-        password=DB_PASSWORD
-        host=DB_HOST_DEBUG
-        port=DB_POST
+        user=DB_USER,
+        password=DB_PASSWORD,
+        host=DB_HOST_DEBUG,
+        port=DB_PORT,
         database=DB_NAME
 )
 
 cur = conn.cursor()
 
 def get_all_feeds():
-        for row in cur.execute("SELECT * FROM Feeds"):
-                print(row)
+        cur.execute("SELECT * FROM Feeds")
+        for (url) in cur:
+                print(url)
+
+def select_feed(feed_url):
+        cur.execute(f"SELECT Url FROM Feeds where Url = '{feed_url}'")
+        return list(cur)
+
+def delete_feed(feed_url):
+        cur.execute(f"DELETE FROM Feeds WHERE Url = '{feed_url}'")
+        conn.commit()
 
 def add_new_feed(feed_url):
         cur.execute(f"INSERT INTO Feeds(Url) VALUES ('{feed_url}');")
